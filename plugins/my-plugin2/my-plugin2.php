9edd973adffa6fa338b2a53fe67bccaaa2ca83dd
Kazuaki Ueda <kazuaki.ueda@gmail.com>
2019-06-02 13:39:39 +0900

    modified spaces

---
+++ plugins/my-plugin2/my-plugin2.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     My Plugin2
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     my-plugin2
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         My_Plugin2
+ */
+
+// Your code starts here.
